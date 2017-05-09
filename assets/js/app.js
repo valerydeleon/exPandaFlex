4e703e7240fb3f17b4bbbc79d4b8f47ac57ea83d
Valeria de Leon <valery.deleon@gmail.com>
2017-05-09 17:00:28 -0500

    funcionalidad origen mostrar y ocultar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/////*  *///////
+/////* borrar *///////
 
 var taches = document.getElementsByClassName("cerrar");
 
@@ -11,6 +11,7 @@ function borrarFoto(){
 }
 
 
+/////* restaurar *///////
 
 var botonDeMostrar = document.getElementById("botonMostrar")
 
@@ -20,4 +21,39 @@ function mostrarFotos(){
   for (var i = 0; i < taches.length; i++ ){
     taches[i].parentNode.style.display="flex";
   }
+}
+
+
+/////* boton Origen *///////
+
+var botonOrigen = document.getElementById("botonOrigen");
+var parrafoOrigen = document.getElementById("parrafoOrigen");
+
+botonOrigen.addEventListener("click", mostrarOrigen);
+botonOrigen.addEventListener("dblclick", ocultarOrigen);
+
+function mostrarOrigen(){
+  parrafoOrigen.style.visibility="visible";
+}
+
+function ocultarOrigen(){
+  parrafoOrigen.style.visibility="hidden";
+}
+
+
+
+/////* boton Extincion *///////
+
+var botonExtincion = document.getElementById("botonExtincion");
+var parrafoExtincion = document.getElementById("parrafoExtincion");
+
+botonExtincion.addEventListener("click", mostrarExtincion);
+botonExtincion.addEventListener("dblclick", ocultarExtincion);
+
+function mostrarExtincion(){
+  parrafoExtincion.style.visibility="visible";
+}
+
+function ocultarExtincion(){
+  parrafoExtincion.style.visibility="hidden";
 }
